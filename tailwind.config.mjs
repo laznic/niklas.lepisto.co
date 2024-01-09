3aaa7d7f37e70cb3004c2bfd494db025f55c8d30
Niklas L <niklas@demtrees.com>
2024-01-10 00:00:01 +0200

    Add more stuff

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,12 +4,13 @@ export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
-    colors: {
-      primary: "hsl(var(--color-primary) / <alpha-value>)",
-      separator: "hsl(var(--color-separator) / <alpha-value>)",
-      background: "hsl(var(--color-background) / <alpha-value>)",
+    extend: {
+      colors: {
+        primary: "hsl(var(--color-primary) / <alpha-value>)",
+        separator: "hsl(var(--color-separator) / <alpha-value>)",
+        background: "hsl(var(--color-background) / <alpha-value>)",
+      },
     },
-    extend: {},
   },
   plugins: [require("@tailwindcss/typography")],
 };
