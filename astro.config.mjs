1cd586819aaad56e44cc444d532f50ea51ae9268
Niklas L <niklas@demtrees.com>
2024-01-09 18:33:00 +0200

    Add initial dark mode support and CSS variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,9 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   prefetch: true,
-  integrations: [tailwind()],
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+  ],
 });
